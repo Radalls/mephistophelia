1fa95a02fa4fb121820c15aa22a8d85e9085e4c4
Radalls <leosallard@gmail.com>
2024-01-30 12:17:59 +0100

    maps

--- assets/maps/tiles.tsx
+++ assets/maps/tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="test_map_tiles" tilewidth="128" tileheight="128" tilecount="156" columns="13">
- <image source="tiles.png" width="1664" height="1536"/>
+<tileset version="1.10" tiledversion="1.10.2" name="tiles" tilewidth="128" tileheight="128" tilecount="25" columns="5">
+ <image source="tiles.png" width="640" height="640"/>
 </tileset>
